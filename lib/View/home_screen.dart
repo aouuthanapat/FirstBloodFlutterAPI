68a8de287df8dbb569ec96e84b7edcae1800ca45
aouuthanapat <daniilkuratsky16@gmail.com>
2024-02-08 13:26:17 +0300

    main cards UI design (4) ?47

--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -1,8 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_api/Model/news_channel_headlines_model.dart';
 import 'package:flutter_api/ViewModel/news_view_model.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,6 +16,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   NewsViewModel newsViewModel = NewsViewModel();
+
+  final format = DateFormat('MMMM dd, yyyy');
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +52,87 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   );
                 } else {
-                  return Container();
+                 return ListView.builder(
+                   itemCount: snapshot.data!.articles!.length,
+                     scrollDirection: Axis.horizontal,
+                     itemBuilder: (context, index) {
+                     DateTime dateTime = DateTime.parse(snapshot.data!.articles![index].publishedAt.toString());
+                       return SizedBox(
+                         child: Stack(
+                           alignment: Alignment.center,
+                           children: [
+                             Container(
+                               height: height * 0.6,
+                               width: width * .9,
+                               padding: EdgeInsets.symmetric(
+                                 horizontal: height * .02
+                               ),
+                               child: ClipRRect(
+                                 borderRadius: BorderRadius.circular(15),
+                                 child: CachedNetworkImage(
+                                   imageUrl: snapshot.data!.articles![index].urlToImage.toString(),
+                                   fit: BoxFit.cover,
+                                   placeholder: (context, url) => Container(child: spinKit2,),
+                                   errorWidget: (context, url, error) => Icon(Icons.error_outline, color: Colors.red,),
+                                 ),
+                               ),
+                             ),
+                             Positioned(
+                               bottom: 20,
+                               child: Card(
+                                 elevation: 5,
+                                 color: Colors.white,
+                                 shape: RoundedRectangleBorder(
+                                   borderRadius: BorderRadius.circular(12),
+                                 ),
+                                 child: Container(
+                                   alignment: Alignment.bottomCenter,
+                                   padding: EdgeInsets.all(15),
+                                   height: height * .22,
+                                   child: Column(
+                                     mainAxisAlignment: MainAxisAlignment.center,
+                                     crossAxisAlignment: CrossAxisAlignment.center,
+                                     children: [
+                                       Container(
+                                         width: width * 0.7,
+                                         child: Text(snapshot.data!.articles![index].title.toString(),
+                                           maxLines: 2,
+                                           overflow: TextOverflow.ellipsis,
+                                           style:
+                                           GoogleFonts.poppins(fontSize: 17, fontWeight: FontWeight.w700),
+                                           ),
+                                       ),
+                                       Spacer(),
+                                       Container(
+                                         width: width * 0.7,
+                                         child: Row(
+                                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                           children: [
+                                             Text(snapshot.data!.articles![index].source!.name.toString(),
+                                               maxLines: 2,
+                                               overflow: TextOverflow.ellipsis,
+                                               style:
+                                               GoogleFonts.poppins(fontSize: 13, fontWeight: FontWeight.w600),
+                                             ),
+                                             Text(format.format(dateTime),
+                                               maxLines: 2,
+                                               overflow: TextOverflow.ellipsis,
+                                               style:
+                                               GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w500),
+                                             )
+                                           ],
+                                         ),
+                                       )
+                                     ],
+                                   ),
+                                 ),
+                               ),
+                             )
+                           ],
+                         ),
+                       );
+                     }
+                 );
                 }
               },
             ),
@@ -58,3 +142,7 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+const spinKit2 = SpinKitFadingCircle(
+  color: Colors.amber,
+  size: 50,
+);
